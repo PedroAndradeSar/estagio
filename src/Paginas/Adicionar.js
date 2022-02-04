a256bc4e0f7362e504d83d4da2d4101214e89591
Pedro Andrade Sartorato <phandradesar@hotmail.com>
2022-02-04 18:12:46 -0300

    04-02-2022

--- src/Paginas/Adicionar.js
+++ src/Paginas/Adicionar.js
@@ -1,53 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Paginas/Adicionar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Paginas/Adicionar.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-const Adicionarpt   = () => {
-    return (
+const Adicionarpt = () => {
+  return (
     <div>
       <div>
-        <header>        
-          <nav className='caminho'>       
-              <Link to="/home">Home &gt;{" "} </Link>
-              <Link to="/carrinho">Carrinho &gt;{" "} </Link>
-              <Link to="/editar">Editar &gt;{" "}</Link>
+        <header>
+          <nav className="caminho">
+            <Link to="/home">Home &gt; </Link>
+            <Link to="/carrinho">Carrinho &gt; </Link>
+            <Link to="/editar">Editar &gt; </Link>
           </nav>
           <h1>Adicionar Produto</h1>
         </header>
       </div>
-        
 
-
-        <div class="container">
-          <fieldset class="fieldset-border">
-            <legend class="legend-border">Nome do Produto</legend>
-          </fieldset>
-          <fieldset class="fieldset-border">
-            <legend class="legend-border">Marca</legend>
-          </fieldset>
-          <div className="container-menor">
-            <fieldset class="fieldset-border">
-            <legend class="legend-border">Valor</legend>
-          </fieldset>
-          <fieldset class="fieldset-border">
-            <legend class="legend-border">Cor</legend>
-          </fieldset>
-          </div>
-
-
-        falta data de cadastro
-
-        </div>
-
-        <div className='botao'>
-          <button> Adicionar Produto </button>
-        </div>
-
-
-
+      <div class="container">
+        <fieldset class="fieldset-border">
+          <legend class="legend-border">Nome do Produto</legend>
+        </fieldset>
+        <fieldset class="fieldset-border">
+          <legend class="legend-border">Marca</legend>
+        </fieldset>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        ></Box>
+        <TextField
+          id="outlined-read-only-input"
+          label="Data"
+          defaultValue={"  "}
+          InputProps={{
+            readOnly: true,
+          }}
+        ></TextField>
+       
       </div>
-        ) 
- 
+      {/* <div>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        ></Box>
+        <TextField
+          id="outlined-read-only-input"
+          label="Data"
+          defaultValue={"  "}
+          InputProps={{
+            readOnly: true,
+          }}
+        ></TextField>
+      </div> */}
+
+      <button className="botao"> Adicionar Produto </button>
+      falta data de cadastro
+    </div>
+  );
 };
 
 export default Adicionarpt;
